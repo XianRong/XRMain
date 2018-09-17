05da69fec61a67dcbb1dd8fb5aa2c67d3dc4da5f
贤荣 <>
2018-09-17 10:11:18 +0800

    新增0.1.1版本

--- XRMain/Classes/XRTabBar.m
+++ XRMain/Classes/XRTabBar.m
@@ -23,16 +23,19 @@
 - (void)setup {
     
     // 设置样式, 去除tabbar上面的黑线
-    self.barStyle = UIBarStyleBlack;
+//    self.barStyle = UIBarStyleBlack;
     
     self.translucent = NO;
     
-    // 设置tabbar 背景图片
-    NSBundle *currentBundle = [NSBundle bundleForClass:[self class]];
-    NSString *bundleName = [currentBundle.infoDictionary[@"CFBundleName"] stringByAppendingString:@".bundle"];
-    NSString *path = [currentBundle pathForResource:@"tabbar_bg@3x.png" ofType:nil inDirectory:bundleName];
-    UIImage *image = [UIImage imageWithContentsOfFile:path];
-    self.backgroundImage = image;
+    self.backgroundColor = [UIColor whiteColor];
+    
+//    // 设置tabbar 背景图片
+//    NSBundle *currentBundle = [NSBundle bundleForClass:[self class]];
+//    NSString *bundleName = [currentBundle.infoDictionary[@"CFBundleName"] stringByAppendingString:@".bundle"];
+//    NSString *path = [currentBundle pathForResource:@"tabbar_bg@3x.png" ofType:nil inDirectory:bundleName];
+//    UIImage *image = [UIImage imageWithContentsOfFile:path];
+//    self.backgroundImage = image;
+    
 }
 
 
